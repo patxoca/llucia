06151634382de0963fc79c221e7b8af0ce905f48
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-11-06 21:36:57 +0000

    Afegit paràmetre dimensió a 'enter_a_coalicio' i 'generar_coalicions'.
    git-svn-id: svn://localhost/bdfcee/llucia/trunk@1767 3d8c60a5-753f-46c6-a465-bf878c2d9da3

--- linalg++/tests/test_utils.cc
+++ linalg++/tests/test_utils.cc
@@ -5,24 +5,19 @@
 #include "gtest/gtest.h"
 #include "../utils.h"
 
-TEST(UtilsDecimalACoalicio, valor_de_DIMENSIO_es_6) {
-	/* els tests depenen de que la DIMENSIO sigui 6 */
-	EXPECT_EQ(DIMENSIO, 6);
-}
-
 TEST(UtilsDecimalACoalicio, mida_coalicio_coincideix_amb_DIMENSIO) {
 	Coalicio v;
 
-	v = enter_a_coalicio(3);
-	EXPECT_EQ(v.size(), DIMENSIO);
+	v = enter_a_coalicio(3, 6);
+	EXPECT_EQ(v.size(), 6);
 }
 
 TEST(UtilsDecimalACoalicio, contingut_coalicio_buida) {
 	Coalicio v;
 	Enter e[] = {0, 0, 0, 0, 0, 0};
 
-	v = enter_a_coalicio(0);
-	for (int i = 0; i < DIMENSIO; i++) {
+	v = enter_a_coalicio(0, 6);
+	for (int i = 0; i < 6; i++) {
 		EXPECT_EQ(e[i], v[i]);
 	}
 }
@@ -31,8 +26,8 @@ TEST(UtilsDecimalACoalicio, contingut_coalicio_no_buida) {
 	Coalicio v;
 	Enter e[] = {1, 1, 0, 0, 0, 0};
 
-	v = enter_a_coalicio(3);
-	for (int i = 0; i < DIMENSIO; i++) {
+	v = enter_a_coalicio(3, 6);
+	for (int i = 0; i < 6; i++) {
 		EXPECT_EQ(e[i], v[i]);
 	}
 }
@@ -41,8 +36,8 @@ TEST(UtilsDecimalACoalicio, contingut_coalicio_total) {
 	Coalicio v;
 	Enter e[] = {1, 1, 1, 1, 1, 1};
 
-	v = enter_a_coalicio(63);
-	for (int i = 0; i < DIMENSIO; i++) {
+	v = enter_a_coalicio(63, 6);
+	for (int i = 0; i < 6; i++) {
 		EXPECT_EQ(e[i], v[i]);
 	}
 }
--- linalg++/utils.cc
+++ linalg++/utils.cc
@@ -5,8 +5,8 @@
 #include "utils.h"
 
 
-Coalicio enter_a_coalicio(Enter valor) {
-	Coalicio v = Coalicio(DIMENSIO, 0);
+Coalicio enter_a_coalicio(Enter valor, int dimensio) {
+	Coalicio v = Coalicio(dimensio, 0);
 
 	for (int i = 0; valor != 0 && i < NOMBRE_MAXIM_JUGADORS; i++, valor >>= 1) {
 		v[i] = valor & 1;
@@ -14,10 +14,11 @@ Coalicio enter_a_coalicio(Enter valor) {
 	return v;
 }
 
-std::vector<Coalicio> generar_coalicions() {
-	std::vector<Coalicio> c = std::vector<Coalicio>(NOMBRE_COALICIONS);
+std::vector<Coalicio> generar_coalicions(int dimensio) {
+	int nombre_coalicions = (1 << dimensio) - 1;
+	std::vector<Coalicio> c = std::vector<Coalicio>(nombre_coalicions);
 
-	for (int i = 1; i <= NOMBRE_COALICIONS; i++) {
+	for (int i = 1; i <= nombre_coalicions; i++) {
 		c[i - 1] = enter_a_coalicio(i);
 	}
 	return c;
