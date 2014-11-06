1a3a1fbbba64900911831a988314ecdae4647cc3
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-11-06 21:02:50 +0000

    Afegit programa principal per fer proves.
    git-svn-id: svn://localhost/bdfcee/llucia/trunk@1766 3d8c60a5-753f-46c6-a465-bf878c2d9da3

---
+++ linalg++/prova.cc
@@ -0,0 +1,40 @@
+// -*- coding: utf-8 -*-
+
+// $Id:$
+
+#include <iostream>
+#include <time.h>
+
+#include "combinator.h"
+#include "matrix.h"
+#include "rational.h"
+#include "utils.h"
+
+
+int main(int argc, char **argv) {
+	std::vector<Coalicio> coalicions;
+	Combinator *combinador;
+	Matriu m(DIMENSIO, DIMENSIO);
+	Fraccio resultat;
+	Matriu inversa;
+	clock_t t0, tf;
+	int k = 0;
+
+	coalicions = generar_coalicions();
+	combinador = new Combinator(coalicions.size(), DIMENSIO);
+	t0 = clock();
+	for (const int *c = combinador->first(); c != NULL; c = combinador->next()) {
+		for (int i = 0; i < DIMENSIO; i++) {
+			Coalicio coalicio = coalicions[c[i]];
+			for (int j = 0; j < DIMENSIO; j++) {
+				m.put(i, j, coalicio[j]);
+			}
+		}
+		inversa = m.getInverse();
+		k++;
+	}
+	tf = clock();
+
+	std::cout << k << std::endl << std::endl;
+	std::cout << (tf - t0) / (double)CLOCKS_PER_SEC << "segons\n";
+}
