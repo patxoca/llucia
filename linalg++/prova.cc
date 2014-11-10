3cac1d1e073851fd62f7398897ce344c3d1467f7
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-11-10 15:29:27 +0000

    Redefinit el tipus Coalicio com Enter.
    git-svn-id: svn://localhost/bdfcee/llucia/trunk@1784 3d8c60a5-753f-46c6-a465-bf878c2d9da3

--- linalg++/prova.cc
+++ linalg++/prova.cc
@@ -17,27 +17,38 @@ int main(int argc, char **argv) {
 	Matriu m(DIMENSIO, DIMENSIO);
 	Fraccio resultat;
 	clock_t t0, tf;
-	int num_combinacions = 0;
-	int num_bases = 0;
+	long num_combinacions = 0;
+	long num_bases = 0;
+	long num_no_det = 0;
+	Coalicio or_coalicions;
 
-	coalicions = generar_coalicions(DIMENSIO);
-	combinador = new Combinator(coalicions.size(), DIMENSIO);
+	std::cout << "Iniciant càlcul n = " << DIMENSIO << std::endl;
+
+	combinador = new Combinator(NOMBRE_COALICIONS, DIMENSIO);
 	t0 = clock();
 	for (const int *c = combinador->first(); c != NULL; c = combinador->next()) {
+		or_coalicions = 0;
 		num_combinacions++;
 		for (int i = 0; i < DIMENSIO; i++) {
-			Coalicio coalicio = coalicions[c[i]];
+			Coalicio coalicio = c[i] + 1;
+			or_coalicions |= coalicio;
 			for (int j = 0; j < DIMENSIO; j++) {
-				m.put(i, j, coalicio[j]);
+				m.put(i, j,  coalicio & 1);
+				coalicio >>= 1;
 			}
 		}
-		if (m.det() != 0) {
-			num_bases++;
+		if (or_coalicions == COALICIO_TOTAL) {
+		    if (m.det() != 0) {
+			    num_bases++;
+			}
+		} else {
+			num_no_det++;
 		}
 	}
 	tf = clock();
 
-	std::cout << "Num combinacions: " << num_combinacions << std::endl << std::endl;
-	std::cout << "Num bases:        " << num_bases << std::endl << std::endl;
+	std::cout << "Num combinacions: " << num_combinacions << std::endl;
+	std::cout << "Num bases:        " << num_bases << std::endl;
+	std::cout << "Num no det:       " << num_no_det << std::endl;
 	std::cout << (tf - t0) / (double)CLOCKS_PER_SEC << "segons\n";
 }
