d477c66f6e91b3593e8a483c5686b5c45c246de4
Alexis Roda <alexis.roda.villalonga@gmail.com>
2014-11-13 18:18:47 +0000

    Implementades funcions per comunicar productor i treballadors.
    git-svn-id: svn://localhost/bdfcee/llucia/trunk@1790 3d8c60a5-753f-46c6-a465-bf878c2d9da3

---
+++ linalg++/missatge.cc
@@ -0,0 +1,143 @@
+// -*- coding: utf-8 -*-
+
+// $Id:$
+
+#include <zmq.hpp>
+
+#include "missatge.h"
+
+
+bool msg_request_abort(zmq::socket_t & socket, int worker_id) {
+	zmq::message_t message(sizeof(request_t));
+	zmq::message_t reply;
+	request_t      request;
+
+	request.message = RQ_ABORT;
+	request.worker_id = worker_id;
+
+	memcpy((void*)message.data(), &request, sizeof(request));
+	if (!socket.send(message)) {
+		// error imagino
+	}
+	if (!socket.recv(&reply)) {
+		// error ?
+	}
+	// la resposta hauria de ser ACK, no importa realment
+	return true;
+}
+
+bool msg_request_get(zmq::socket_t & socket, int worker_id, int last_packet_id, int *packet_id, int *size, int *buffer) {
+	zmq::message_t   message(sizeof(request_get_t));
+	zmq::message_t   reply;
+	request_get_t    request;
+	response_data_t *response;
+
+	request.message = RQ_GET;
+	request.worker_id = worker_id;
+	request.last_packet_id = last_packet_id;
+
+	memcpy((void*)message.data(), &request, sizeof(request));
+	if (!socket.send(message)) {
+		// error imagino
+	}
+	if (!socket.recv(&reply)) {
+		// error ?
+	}
+	// la resposta hauria de ser ACK, no importa realment
+	response = (response_data_t*)reply.data();
+	if (response->message != RP_DATA) {
+		// error!!
+	}
+	*packet_id = response->packet_id;
+	*size = response->size;
+	memcpy(buffer, response->packet, response->size * sizeof(int));
+
+	return true;
+}
+
+bool msg_request_register(zmq::socket_t & socket, int *worker_id) {
+	zmq::message_t         message(sizeof(request_t));
+	zmq::message_t         reply;
+	request_t              request;
+	response_registered_t *response;
+
+	request.message = RQ_REG;
+	request.worker_id = -1;
+
+	memcpy((void*)message.data(), &request, sizeof(request));
+	if (!socket.send(message)) {
+		// error imagino
+	}
+	if (!socket.recv(&reply)) {
+		// error ?
+	}
+	// la resposta hauria de ser ACK, no importa realment
+	response = (response_registered_t*)reply.data();
+	if (response->message != RP_REGISTERED) {
+		*worker_id = -1;
+	} else {
+		*worker_id = response->worker_id;
+	}
+	return true;
+}
+
+bool msg_request_unregister(zmq::socket_t & socket, int worker_id) {
+	zmq::message_t  message(sizeof(request_t));
+	zmq::message_t  reply;
+	request_t       request;
+	response_t     *response;
+
+	request.message = RQ_UNREG;
+	request.worker_id = worker_id;
+
+	memcpy((void*)message.data(), &request, sizeof(request));
+	if (!socket.send(message)) {
+		// error imagino
+	}
+	if (!socket.recv(&reply)) {
+		// error ?
+	}
+	// la resposta hauria de ser ACK, no importa realment
+	return true;
+}
+
+bool msg_reply_ack(zmq::socket_t & socket) {
+	zmq::message_t message(sizeof(response_t));
+	response_t     response;
+
+	response.message = RP_ACK;
+	memcpy((void*)message.data(), &response, sizeof(response));
+	if (!socket.send(message)) {
+		// error?
+	}
+	return true;
+}
+
+bool msg_reply_data(zmq::socket_t & socket, int packet_id, int size, const int *data) {
+	zmq::message_t   message(sizeof(response_data_t));
+	response_data_t  response;
+
+	response.message = RP_DATA;
+	response.packet_id = packet_id;
+	response.size = size;
+	assert(size <= DIMENSIO);
+	memcpy(&response.packet, data, size * (sizeof(int)));
+	memcpy((void*)message.data(), &response, sizeof(response));
+	if (!socket.send(message)) {
+		// error?
+	}
+	return true;
+}
+
+bool msg_reply_registered(zmq::socket_t & socket, int worker_id) {
+	zmq::message_t         message(sizeof(response_registered_t));
+	response_registered_t  response;
+
+	response.message = RP_REGISTERED;
+	response.worker_id = worker_id;
+	memcpy((void*)message.data(), &response, sizeof(response));
+	if (!socket.send(message)) {
+		// error?
+	}
+	return true;
+}
---
+++ linalg++/missatge.h
@@ -0,0 +1,97 @@
+// -*- coding: utf-8 -*-
+
+// $Id:$
+
+#ifndef _MISSATGE_H_
+#define _MISSATGE_H_
+
+
+#include "tipus.h"
+
+// Cada treballador té associat un identificador únic.
+//
+// workflow: el treballador envia una petició i el coordinador retorna una
+// resposta (sempre).
+//
+// Tipus de missatges (i paràmetres):
+//
+// - ABORT: id_treballador
+//   un treballador detecta una condició per la que no cal continuar el
+//   càlcul. El productor respon ACK (acknowledgement) al client i a partir
+//   d'eixe moment enviarà paquets de finalització en les peticions GET dels
+//   clients.
+//
+// - GET: id_treballador, id_paquet_anterior
+//   un treballador sol·licita un paquet de dades i informa de la finalització
+//   del càlcul amb el paquet prèvi. Si és la primera petició (no hi ha paquet
+//   prèvi) id_paquet_anterior == -1
+//
+//   El productor respon amb un paquet DATA que conté el id del paquet, el
+//   nombre de dades i les dades. Si no hi ha més dades que processar o s'ha
+//   avortat el càlcul el productor respondra amb un paquet de finalització
+//   (un paquet amb ID -1).
+//
+// - REG:
+//   un treballador demana registrar-se
+//
+//   Si s'accepta el registre el productor respon amb REGISTERED i envia al
+//   treballador el seu ID. Si es rebutja respon amb REJECT.
+//
+// - UNREG: id_treballador
+//   un treballador es desregistra. El productor respont amb ACK (ok) o REJECT
+//   (error).
+//
+//   Quan no queden treballadors registrats el productor finalitza.
+
+typedef enum {
+	RQ_ABORT,
+	RQ_GET,
+	RQ_REG,
+	RQ_UNREG
+} request_type_t;
+
+typedef enum {
+	RP_ACK,
+	RP_DATA,
+	RP_REGISTERED,
+	RP_REJECT,
+} response_type_t;
+
+typedef struct {
+	request_type_t message;
+	int            worker_id;
+} request_t;
+
+typedef struct : request_t {
+	int   last_packet_id;
+} request_get_t;
+
+
+typedef struct {
+	response_type_t message;
+} response_t;
+
+typedef struct : response_t {
+	int   packet_id;         // ID del paquet, assignat pel productor, -1 pels
+							 // paquets de finalització
+	int   size;              // nombre d'elements en el paquet
+	int   packet[DIMENSIO];  // paquet
+} response_data_t;
+
+typedef struct : response_t {
+	int   worker_id;         // ID del treballador, -1 si rebutjat
+} response_registered_t;
+
+
+
+bool msg_request_abort(zmq::socket_t & socket, int worker_id);
+bool msg_request_get(zmq::socket_t & socket, int worker_id, int last_packet_id, int *packet_id, int *size, int *buffer);
+bool msg_request_register(zmq::socket_t & socket, int *worker_id);
+bool msg_request_unregister(zmq::socket_t & socket, int worker_id);
+
+
+bool msg_reply_ack(zmq::socket_t & socket);
+bool msg_reply_data(zmq::socket_t & socket, int packet_id, int size, const int *data);
+bool msg_reply_registered(zmq::socket_t & socket, int worker_id);
+
+#endif /* _MISSATGE_H_ */
